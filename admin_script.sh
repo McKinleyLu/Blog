9e8ccfeacf0c11f754301864558a89f016565061
mckinleylu <lt981109@gmail.com>
2022-06-19 17:42:15 +0800

    ubunte

--- admin_script.sh
+++ admin_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
-hexo clean
-hexo g  -d
 git remote add esc git@github.com:McKinleyLu/Blog.git
-git pull esc esc  
+git pull esc esc
 git rm -r --cached .
 git checkout esc
 git add .
 git commit -m "ubunte"
 git push  origin   esc:esc
+hexo clean
+hexo g  -d
